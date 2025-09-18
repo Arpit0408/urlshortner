a8f6c6627e36836817a204040bbb4dadd200bdf2
Arpit Paliwal <133906733+Arpit0408@users.noreply.github.com>
2025-09-18 23:35:35 +0530

    Update axiosInstace.js

--- frontend/src/utils/axiosInstace.js
+++ frontend/src/utils/axiosInstace.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     baseURL:"https://urlshortner-k7bj.onrender.com",
-    timeout:30000,
-  withCredentials: true
+timeout: 30000, 
+    withCredentials: true
 });
 
 export default axiosInstance;
