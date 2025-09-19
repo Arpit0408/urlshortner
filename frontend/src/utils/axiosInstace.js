e938a10136bd2b2fb55128d77f228602290b81b4
Arpit Paliwal <133906733+Arpit0408@users.noreply.github.com>
2025-09-19 10:39:17 +0530

    Update axiosInstace.js

--- frontend/src/utils/axiosInstace.js
+++ frontend/src/utils/axiosInstace.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL:"https://urlshortner-k7bj.onrender.com",
-timeout: 30000, 
-    withCredentials: true
+    baseURL: "https://urlshortner-k7bj.onrender.com", // ✅ Base URL
+    timeout: 30000, // ✅ 30 second timeout
+    withCredentials: true // ✅ Cookies allow karega (if CORS allows it)
 });
 
 export default axiosInstance;
